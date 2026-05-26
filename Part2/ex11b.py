4059346b7027bf5d33bff0af42169237451272e7
mikokonut <tyijing8@gmail.com>
2026-05-26 03:15:27 +0000

    done

---
+++ Part2/ex11b.py
@@ -0,0 +1,16 @@
+def calculate(num1, operator, num2):
+   if operator == "+":
+      return (num1+num2)
+   elif operator == "-":
+     return (num1-num2)
+   elif operator == "*":
+     return (num1*num2)
+   elif operator == "/":
+     return (num1/num2)
+   else:
+     return 'Invalid operator'
+   
+print(calculate(10, "+", 10))  # should return 20
+print(calculate(10, "-", 10))  # should return 0
+print(calculate(10, "*", 10))  # should return 100
+print(calculate(10, "/", 10))  # should return 1.0
---
+++ Part2/ex16.py
@@ -0,0 +1,5 @@
+name = input ("What is your name?\n")
+food = input ("What is your favourite food?\n")
+name="Marvin"
+food="cendol"
+print(f"Hi {name},I heard you love {food}")
